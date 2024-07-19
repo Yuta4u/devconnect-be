0e94354b7416ac4f9f9e332b248bbca37943314b
Yuta4u <yudhistira4u@gmail.com>
2024-07-20 02:32:10 +0700

    lupa import serverlessnya coy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 require("dotenv").config()
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
+const serverless = require("serverless-http")
 
 // routes
 const devRoutes = require("./routes/devRouter")
